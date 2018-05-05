7aa9a1ca441d528e0c7e1da225f9d992d25a6a3e
Felipeek <felipeek@hotmail.com>
2018-05-05 18:19:25 -0300

    adding third output window

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,12 +11,18 @@
 using namespace cv;
 using namespace std;
  
-Mat filterCameraFrame(Mat cameraFrame, float* image_data, float* result)
-{
-    float ss = 20.0f;
-    float sr = 1.0f;
-    int num_iterations = 3;
+#define NUM_THREADS 16
+#define Y_BLOCKS 4
+#define X_BLOCKS 4
+#define S_S 5.0f
+#define S_R 0.6f
+#define NUM_ITERATIONS 2
+#define DETAIL_HIGHLIGHT_FACTOR 1.8f
 
+void filterCameraFrame(Mat cameraFrame, Mat* filteredFrame, Mat* detailedFrame,
+    float* image_data, float* filteredResult, float* detailedResult)
+{
+    // Transform input image to float
     for (int i = 0; i < cameraFrame.rows; ++i)
         for (int j = 0; j < cameraFrame.cols; ++j)
         {
@@ -26,26 +32,37 @@ Mat filterCameraFrame(Mat cameraFrame, float* image_data, float* result)
             image_data[4 * cameraFrame.cols * i + 4 * j + 3] = 1.0f;
         }
 
+    // Filter image
     start_clock();
-    s32 threads = 8;
-    s32 x = 1;
-    s32 y = 16;
-    parallel_domain_transform(image_data, cameraFrame.cols, cameraFrame.rows, 4, ss, sr, num_iterations,
-        threads, x, y, result);
-    //std::copy(image_data, image_data + cameraFrame.cols * cameraFrame.rows * 4, result);
+    parallel_domain_transform(image_data, cameraFrame.cols, cameraFrame.rows, 4, S_S, S_R, NUM_ITERATIONS,
+        NUM_THREADS, X_BLOCKS, Y_BLOCKS, filteredResult);
     r32 total_time = end_clock();
-
 	print("Total time: %.3f seconds\n", total_time);
 
+    // Apply R = (I - S) * DHF + I
     for (int i = 0; i < cameraFrame.rows; ++i)
         for (int j = 0; j < cameraFrame.cols; ++j)
         {
-            cameraFrame.data[3 * cameraFrame.cols * i + 3 * j + 0] = (u8)roundf(result[4 * cameraFrame.cols * i + 4 * j + 0] * 255.0f);
-            cameraFrame.data[3 * cameraFrame.cols * i + 3 * j + 1] = (u8)roundf(result[4 * cameraFrame.cols * i + 4 * j + 1] * 255.0f);
-            cameraFrame.data[3 * cameraFrame.cols * i + 3 * j + 2] = (u8)roundf(result[4 * cameraFrame.cols * i + 4 * j + 2] * 255.0f);
+            r32 r, g, b;
+            r = (image_data[4 * cameraFrame.cols * i + 4 * j + 0] - filteredResult[4 * cameraFrame.cols * i + 4 * j + 0]) *
+                DETAIL_HIGHLIGHT_FACTOR + image_data[4 * cameraFrame.cols * i + 4 * j + 0];
+            g = (image_data[4 * cameraFrame.cols * i + 4 * j + 1] - filteredResult[4 * cameraFrame.cols * i + 4 * j + 1]) *
+                DETAIL_HIGHLIGHT_FACTOR + image_data[4 * cameraFrame.cols * i + 4 * j + 1];
+            b = (image_data[4 * cameraFrame.cols * i + 4 * j + 2] - filteredResult[4 * cameraFrame.cols * i + 4 * j + 2]) *
+                DETAIL_HIGHLIGHT_FACTOR + image_data[4 * cameraFrame.cols * i + 4 * j + 2];
+            
+            r = MIN(1.0f, MAX(0.0f, r));
+            g = MIN(1.0f, MAX(0.0f, g));
+            b = MIN(1.0f, MAX(0.0f, b));
+
+            detailedResult[4 * cameraFrame.cols * i + 4 * j + 0] = r;
+            detailedResult[4 * cameraFrame.cols * i + 4 * j + 1] = g;
+            detailedResult[4 * cameraFrame.cols * i + 4 * j + 2] = b;
         }
 
-    return cameraFrame;
+    // Fill Mats
+    *filteredFrame = cv::Mat(cameraFrame.rows, cameraFrame.cols, CV_32FC4, filteredResult);
+    *detailedFrame = cv::Mat(cameraFrame.rows, cameraFrame.cols, CV_32FC4, detailedResult);
 }
 
 int main() {
@@ -59,30 +76,37 @@ int main() {
 
     print("width: %d, height: %d\n", width, height);
 
+    // Pre-alloc memory
     float *image_data = (float *)malloc(4 * sizeof(float) * width * height);
-    float *result = (float *)malloc(4 * sizeof(float) * width * height);
+    float *filteredResult = (float *)malloc(4 * sizeof(float) * width * height);
+    float *detailedResult = (float *)malloc(4 * sizeof(float) * width * height);
 
     if (!stream1.isOpened()) { //check if video device has been initialised
         cout << "cannot open camera";
     }
 
+    // Create windows
     namedWindow("input");
     moveWindow("input", 0,0);
-
-    namedWindow("output");
-    moveWindow("output", width,0);
+    namedWindow("output1");
+    moveWindow("output1", width,0);
+    namedWindow("output2");
+    moveWindow("output2", width,100);
     
     while (true) {
-        Mat cameraFrame;
+        Mat cameraFrame, filteredFrame, detailedFrame;
         stream1.read(cameraFrame);
+        filterCameraFrame(cameraFrame, &filteredFrame, &detailedFrame, image_data, filteredResult, detailedResult);
         imshow("input", cameraFrame);
-        cameraFrame = filterCameraFrame(cameraFrame, image_data, result);
-        imshow("output", cameraFrame);
+        imshow("output1", filteredFrame);
+        imshow("output2", detailedFrame);
         if (waitKey(30) >= 0)
         break;
     }
 
+    // Free memory
     free(image_data);
-    free(result);
+    free(filteredResult);
+    free(detailedResult);
     return 0;
 }
